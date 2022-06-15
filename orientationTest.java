6f003e590d899054557b168d2041f0a3c18b3ad4
Xufeng (Caesar) Dai <xdai1@haverford.edu>
2022-06-15 10:53:55 -0400

    formatting

--- orientationTest.java
+++ orientationTest.java
@@ -16,36 +16,34 @@ class GFG {
 	// 0 --> p, q and r are collinear
 	// 1 --> Clockwise
 	// 2 --> Counterclockwise
-	public static int orientation(Point p1, Point p2,
-										Point p3)
-	{
+	public static int orientation(Point p1, Point p2, Point p3) {
 		// See 10th slides from following link
 		// for derivation of the formula
-		int val = (p2.y - p1.y) * (p3.x - p2.x) -
-				(p2.x - p1.x) * (p3.y - p2.y);
+		int val = (p2.y - p1.y) * (p3.x - p2.x) - (p2.x - p1.x) * (p3.y - p2.y);
 	
-		if (val == 0) return 0; // collinear
+		if (val == 0) {
+			return 0; 
+		} // collinear
 	
 		// clock or counterclock wise
 		return (val > 0)? 1: 2;
 	}
 	
 	/* Driver program to test above function */
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 			Point p1 = new Point(0, 0);
 			Point p2 = new Point(4, 4);
 			Point p3 = new Point(1, 2);
 			
 			int o = orientation(p1, p2, p3);
 			
-			if (o==0)	
+			if (o == 0)	
 			System.out.print("Linear");
 			else if (o == 1)
 			System.out.print("Clockwise");
 			else			
 			System.out.print("CounterClockwise");
-		
+
 	}
 }
 
