8f286469109dc082163687d35e9f98d50871ba5b
Xufeng (Caesar) Dai <xdai1@haverford.edu>
2022-06-16 14:21:38 -0400

    Draw spokes

--- Display.java
+++ Display.java
@@ -34,7 +34,7 @@ public class Display extends JFrame implements MouseListener {
 		c.addMouseListener(this);
 
 		add(c);
-		setSize(1400, 900);
+		setSize(800, 600);
 
         // add windows closer
         addWindowListener (new WindowAdapter() { 
@@ -56,8 +56,8 @@ public class Display extends JFrame implements MouseListener {
 		int x, y;
 		x = e.getX();
 		y = e.getY();
-                Point newPoint = new Point(x, y);
-		this.sites.add(newPoint);
+
+        Point newPoint = new Point(x, y);
 		this.hull.add(newPoint);
 
                 // compute convex hull on sites
@@ -68,6 +68,9 @@ public class Display extends JFrame implements MouseListener {
 				System.out.println(p);
 			}
 		}
+
+		if(!this.hull.contains(newPoint))
+			this.sites.add(newPoint);
 
                 // draw on canvas
                 this.draw(g);
@@ -96,7 +99,11 @@ public class Display extends JFrame implements MouseListener {
             // draw in the site
             for(Point p : this.sites) {
                 g.fillOval(p.x, p.y, 5, 5);
-                System.out.println("(" + p.x + ", " + p.y + ")"); // print out mouse click coordinates
+                System.out.println("(" + p.x + ", " + p.y + ") site"); // print out mouse click coordinates
+            }
+			for(Point p : this.hull) {
+                g.fillOval(p.x, p.y, 5, 5);
+                System.out.println("(" + p.x + ", " + p.y + ") hull"); // print out mouse click coordinates
             }
 
             // draw the convex hull if it can be created
@@ -110,7 +117,80 @@ public class Display extends JFrame implements MouseListener {
             }
 
             // draw the spokes
+			if (hull.size() >= 3 && sites.size() >= 1) {
+				drawSpokes(g);
+			}
         }
+
+		private void drawSpokes(Graphics g) {
+			System.out.println("Drawing Spokes\n");
+			// computes line euqations between hull vertex and sites
+			for (int s = 0; s < sites.size(); s++) {
+				for (int h = 0; h < hull.size() - 1; h++) {
+					int b;
+					double a1, a2, c1, c2;
+
+					// a1 hull to hull
+					a1 = (hull.get(h).y - hull.get((h + 1) % hull.size()).y) / (hull.get(h).x - hull.get((h + 1) % hull.size()).x);
+					
+					// a2 hull edge to site
+					a2 = (hull.get(h).y - sites.get(s).y) / (hull.get(h).x - sites.get(s).x);
+					
+					b = -1;
+					c1 = a1 * hull.get(h).x - hull.get(h).y;
+					c2 = a2 * hull.get(h).x - hull.get(h).y;
+
+					double[] sol = computeLine(a1, a2, b, c1, c2); // returns sol double[]
+
+					if (sol == null) {
+						continue;
+					}
+
+					for (int i = 0; i < this.hull.size(); i++) {
+						Point p1 = this.hull.get(i);
+						Point p2 = this.hull.get( (i+1) % this.hull.size() );
+
+						// determine how the points are positioned
+						int x1, x2, y1, y2;
+						if(p1.x < p2.x) {
+							x1 = p1.x;
+							x2 = p2.x;
+						} else {
+							x2 = p1.x;
+							x1 = p2.x;
+						}
+
+						if(p1.y < p2.y) {
+							y1 = p1.y;
+							y2 = p2.y;
+						} else {
+							y2 = p1.y;
+							y1 = p2.y;
+						}
+
+						// check if solution is inside the rectangle
+						if( (sol[0] > x1 && sol[0] < x2) && (sol[1] > y1 && sol[1] < y2) ) {
+							g.drawLine(this.hull.get(h).x, this.hull.get(h).y, (int) sol[0], (int) sol[1]);
+						}	
+					}
+				}
+			}
+		}
+
+		private double[] computeLine(double a1, double a2, int b, double c1, double c2) {
+			double x, y, k;
+
+			if (a1 == a2) {
+				return null;
+			}
+			
+			k = a1 / a2;
+			y = (c2 - k * c1) / (b - k * b);
+			x = (c1 - b * y) / a1;
+
+			double[] sol = {x, y};
+			return sol;
+		}
   
 	// main
 	public static void main(String args[]) {
