2a502aa6ca59400ef26c327ea5c4c959d1e2a2c5
sam-munoz <smunoz23@colby.edu>
2022-06-17 09:05:52 -0500

    dealt with all boundary cases

--- Display.java
+++ Display.java
@@ -28,10 +28,18 @@ public class Display extends JFrame implements MouseListener {
                 this.hull = new ArrayList<Point>();
 
                 // debuggging
+                // this.sites.add(new Point(200, 200));
+                // this.hull.add(new Point(100, 100));
+                // this.hull.add(new Point(500, 100));
+                // this.hull.add(new Point(500, 500));
+                // this.hull.add(new Point(100, 500));
+                // this.hull.add(new Point(100, 100));
+
                 // this.sites.add(new Point(400, 300));
                 // this.hull.add(new Point(400, 100));
                 // this.hull.add(new Point(700, 300));
-                // this.hull.add(new Point(400, 500));
+                // this.hull.add(new Point(500, 500));
+                // this.hull.add(new Point(300, 500));
                 // this.hull.add(new Point(100, 300));
                 // this.hull.add(new Point(400, 100));
 
@@ -148,13 +156,40 @@ public class Display extends JFrame implements MouseListener {
 
                     // check if our line is vertical
                     if(sitePoint.x == hullPoint.x) {
-                        // loop through all line segements and find the one whose x-coordinates include sitePoint.x
                         System.out.println("vertical site-hull spoke");
-                        return;
-                    } else { // if line is not vertical
-                        m1 = ((double) (sitePoint.y - hullPoint.y)) / ((double) (sitePoint.x - hullPoint.x));
-                        c1 = m1 * hullPoint.x - hullPoint.y;
+                        // return;
+
+                        for(int index = 0; index < this.hull.size() - 1; index++) {
+                            Point h1 = this.hull.get(index);
+                            Point h2 = this.hull.get(index + 1);
+
+                            // determine order of x-coordinates
+                            int x1, x2;
+                            if(h1.x < h2.x) {
+                                x1 = h1.x;
+                                x2 = h2.x;
+                            } else {
+                                x2 = h1.x;
+                                x1 = h2.x;
+                            }
+
+                            if(sitePoint.x >= x1 && sitePoint.x <= x2) {
+                                // check if site is not the vertex we are drawing a spoke from
+                                if(!h1.equals(hullPoint) && !h2.equals(hullPoint)) {
+                                    double x = sitePoint.x;
+                                    double m = ((double) h2.y - h1.y) / ((double) h2.x - h1.x);
+                                    double y = m * x - (m * h1.x - h1.y);
+                                    g.drawLine(hullPoint.x, hullPoint.y, (int) x, (int) y);
+                                    System.out.println("drew points between line-segment (" + h1.x + ", " + h1.y + ")-(" + h2.x + ", " + h2.y + ")" );
+                                    break;
+                                }
+                            }
+                        }
+                        continue;
                     }
+                    // if line is not vertical
+                    m1 = ((double) (sitePoint.y - hullPoint.y)) / ((double) (sitePoint.x - hullPoint.x));
+                    c1 = m1 * hullPoint.x - hullPoint.y;
                     
                     // loop through all hull line segements
                     for(int index = 0; index < this.hull.size() - 1; index++) {
@@ -162,54 +197,61 @@ public class Display extends JFrame implements MouseListener {
                         Point h1 = hull.get(index);
                         Point h2 = hull.get(index + 1);
 
-                        // store solution
+                        // variable to hold our intersection point
                         double[] sol = new double[2];
 
-                        m2 = ((double) (h2.y - h1.y)) / ((double) (h2.x - h1.x));
+                        // if our line segment is vertical
+                        if(h1.x == h2.x) {
+                            sol[0] = h1.x;
+                            sol[1] = m1 * h1.x - c1;
+                        } else {
+                            m2 = ((double) (h2.y - h1.y)) / ((double) (h2.x - h1.x));
+                            c2 = m2 * h1.x - h1.y;
 
-                        c2 = m2 * h1.x - h1.y;
+                            System.out.println("slope for hull-site: " + m1);
+                            System.out.println("slope for hull-hull: " + m2);
 
-                        // System.out.println("slope for hull-site: " + m1);
-                        // System.out.println("slope for hull-hull: " + m2);
+                            // if we have parallel lines
+                            if(m1 == m2) {
+                                System.out.println("parallel lines");
+                                continue;
+                            }
 
-                        // if we have parallel lines
-                        if(m1 == m2) {
-                            System.out.println("parallel lines");
+                            sol = computeLine(m1, m2, c1, c2); // returns solution to system of equations
+
+                            System.out.println("(" + sol[0] + ", " + sol[1] + ") for line (" + h1.x + ", " + h1.y + ")-(" + h2.x + ", " + h2.y +  ")");
+                        }
+
+                        // check if the solution is the vertex we are drawing the spoke
+                        if(sol[0] == (double) hullPoint.x && sol[1] == (double) hullPoint.y) {
                             continue;
                         }
 
-                        sol = computeLine(m1, m2, c1, c2); // returns solution to system of equations
-
-                        System.out.println("(" + sol[0] + ", " + sol[1] + ") for line (" + h1.x + ", " + h1.y + ")-(" + h2.x + ", " + h2.y +  ")");
-
-                        for (int i = 0; i < this.hull.size(); i++) {
-                            Point p1 = this.hull.get(i);
-                            Point p2 = this.hull.get( (i+1) % this.hull.size() );
-
-                            // determine how the points are positioned
-                            int x1, x2, y1, y2;
-                            if(p1.x < p2.x) {
-                                x1 = p1.x;
-                                x2 = p2.x;
-                            } else {
-                                x2 = p1.x;
-                                x1 = p2.x;
-                            }
-
-                            if(p1.y < p2.y) {
-                                y1 = p1.y;
-                                y2 = p2.y;
-                            } else {
-                                y2 = p1.y;
-                                y1 = p2.y;
-                            }
-
-                            // check if solution is inside the rectangle
-                            if( (sol[0] >= x1 && sol[0] <= x2) && (sol[1] >= y1 && sol[1] <= y2) ) {
-                                g.drawLine(this.hull.get(h).x, this.hull.get(h).y, (int) sol[0], (int) sol[1]);
-                            }	
+                        // determine how the points are positioned
+                        int x1, x2, y1, y2;
+                        if(h1.x < h2.x) {
+                            x1 = h1.x;
+                            x2 = h2.x;
+                        } else {
+                            x2 = h1.x;
+                            x1 = h2.x;
                         }
+
+                        if(h1.y < h2.y) {
+                            y1 = h1.y;
+                            y2 = h2.y;
+                        } else {
+                            y2 = h1.y;
+                            y1 = h2.y;
+                        }
+
+                        // check if solution is inside the rectangle
+                        if( (sol[0] >= x1 && sol[0] <= x2) && (sol[1] >= y1 && sol[1] <= y2) ) {
+                            g.drawLine(this.hull.get(h).x, this.hull.get(h).y, (int) sol[0], (int) sol[1]);
+                            // break;
+                        }	
                     }
+
                 }
             }
             System.out.println("Finished drawing spokes");
