ebb1bedc8baebb98a8df6587323d1d9b585e27f4
Student <student@wireless-10-104-58-163.umd.edu>
2022-07-18 11:18:47 -0400

    Package for trapezoidal map

---
+++ src/trapmap/graph/YNode.java
@@ -0,0 +1,27 @@
+package micycle.trapmap.graph;
+
+import micycle.trapmap.Segment;
+
+/**
+ * A y-node stores a segment.
+ * 
+ * @author Tyler Chenhall
+ */
+public class YNode extends Node {
+
+	private Segment data;
+
+	public YNode(Segment s) {
+		super();
+		data = s;
+	}
+
+	/**
+	 * Return the segment data held by this Node
+	 * 
+	 * @return the segment data
+	 */
+	public Segment getData() {
+		return data;
+	}
+}
---
+++ src/trapmap/graph/package-info.java
@@ -0,0 +1,21 @@
+/**
+ * <h3>Package</h3>
+ * 
+ * Components of the <i>History Graph</i>, a Directed Acyclical Graph (DAG) that
+ * stores trapezoids in nodes. This DAG is navigated during point location
+ * queries to find the trapezoid that contains the query point. The root
+ * corresponds to the bounding box (modelled as a trapezoid) of the line
+ * segments.
+ * 
+ * 
+ * <h3>Queries</h3>
+ * 
+ * <li>The query point q is given by its coordinates</li>
+ * <li>If the current node is a leaf, then we are done</li>
+ * <li>Otherwise, one of the descendants of the current trapezoid is a trapezoid
+ * that contains q</li>
+ * <li>Since there are at most 4 descendants, we can find it in O(1) time</li>
+ * <li>Go down to this descendant and repeat the process</li>
+ * 
+ */
+package micycle.trapmap.graph;
