e36e36f825c365f6c66c3e62cf6ccf8ba9813da8
Xufeng (Caesar) Dai <71913201+caesardai@users.noreply.github.com>
2022-07-27 10:48:55 -0400

    Add files via upload

---
+++ src/trapmap/graph/XNode.java
@@ -0,0 +1,27 @@
+package micycle.trapmap.graph;
+
+import processing.core.PVector;
+
+/**
+ * An X node stores a segment end point.
+ * 
+ * @author Tyler Chenhall
+ */
+public class XNode extends Node {
+	
+	private PVector data;
+
+	public XNode(PVector p) {
+		super();
+		data = p;
+	}
+
+	/**
+	 * Return the Point contained in the Node
+	 * 
+	 * @return The Point data
+	 */
+	public PVector getData() {
+		return data;
+	}
+}
